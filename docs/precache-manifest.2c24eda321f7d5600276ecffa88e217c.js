2521d830e325daa5a8b8525ea8cb2ba49271eafb
Carlos Tello <ctello@nativapps.co>
2020-08-04 18:55:04 -0500

    docs added

---
+++ docs/precache-manifest.2c24eda321f7d5600276ecffa88e217c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182974487b411266569115eaa2eade92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f284a27d1ab56c0b0d",
+    "url": "/static/css/main.33aa2db3.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10f284a27d1ab56c0b0d",
+    "url": "/static/js/main.7ad0f3ff.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
